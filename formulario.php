04547bf6f3bf86da1ca45ed2c1addb47e16c14d9
thewolf3304 <santiago.barboza@tecsup.edu.pe>
2023-04-20 13:28:57 -0500

    Final Santiago

--- formulario.php
+++ formulario.php
@@ -4,18 +4,24 @@
 <?php include_once "encabezado.php" ?>
 <div class="row">
     <div class="col-12" style="background-color: white; border-radius: 20px;">
-        <h1>Agregar</h1>
+        <div style="width: 100%; text-align: center;">
+            <h1>Agregar</h1>
+        </div>
         <form action="insertar.php" method="POST">
             <div class="form-group">
-                <label for="nombre">Nombre</label>
-                <input required name="nombre" type="text" id="nombre" placeholder="Nombre de mascota" class="form-control">
+                <label for="nombre">Nombre:</label>
+                <input required name="nombre" type="text" id="nombre" placeholder="Nombre de mascota"
+                    class="form-control">
             </div>
             <div class="form-group">
-                <label for="edad">Edad</label>
+                <label for="edad">Edad:</label>
                 <input required name="edad" type="number" id="edad" placeholder="Edad de mascota" class="form-control">
             </div>
-            <button type="submit" class="btn btn-success">Guardar</button>
-            <a href="./listar.php" class="btn btn-warning">Ver todas</a>
+            <div class="form-group" style="width: 100%; align-items: center; text-align: center;">
+                <button type="submit" class="btn btn-success">Guardar</button>
+                <a href="./listar.php" class="btn btn-warning">Ver todas</a>
+            </div>
+
         </form>
     </div>
 </div>
